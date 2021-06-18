8a792221871e74ea652810ce9c65aec6167026d7
madhead <siarhei.krukau@gmail.com>
2021-06-18 02:00:52 +0200

    Stub for "Editor" achievment processor

---
+++ achievements/editor/src/main/kotlin/by/jprof/telegram/bot/achievements/editor/EditorAchievementUpdateProcessor.kt
@@ -0,0 +1,24 @@
+package by.jprof.telegram.bot.achievements.editor
+
+import by.jprof.telegram.bot.core.UpdateProcessor
+import dev.inmo.tgbotapi.bot.RequestsExecutor
+import dev.inmo.tgbotapi.types.message.abstracts.FromUserMessage
+import dev.inmo.tgbotapi.types.update.abstracts.BaseEditMessageUpdate
+import dev.inmo.tgbotapi.types.update.abstracts.Update
+import org.apache.logging.log4j.LogManager
+
+class EditorAchievementUpdateProcessor(
+    private val bot: RequestsExecutor,
+) : UpdateProcessor {
+    companion object {
+        private val logger = LogManager.getLogger(EditorAchievementUpdateProcessor::class.java)!!
+    }
+
+    override suspend fun process(update: Update) {
+        @Suppress("NAME_SHADOWING")
+        val update = (update as? BaseEditMessageUpdate) ?: return
+        val message = (update.data as? FromUserMessage) ?: return
+
+        logger.debug("{} edited a message", message.user.id)
+    }
+}
