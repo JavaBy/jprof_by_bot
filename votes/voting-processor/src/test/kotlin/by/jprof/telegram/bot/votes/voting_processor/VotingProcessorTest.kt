1b5a001b96296b4a4c1b11793ea6136add5e4b71
madhead <siarhei.krukau@gmail.com>
2021-06-30 01:06:17 +0200

    Allow to process InlineMessageIdDataCallbackQuery in VotingProcessor

--- votes/voting-processor/src/test/kotlin/by/jprof/telegram/bot/votes/voting_processor/VotingProcessorTest.kt
+++ votes/voting-processor/src/test/kotlin/by/jprof/telegram/bot/votes/voting_processor/VotingProcessorTest.kt
@@ -3,20 +3,24 @@ package by.jprof.telegram.bot.votes.voting_processor
 import by.jprof.telegram.bot.votes.dao.VotesDAO
 import by.jprof.telegram.bot.votes.model.Votes
 import by.jprof.telegram.bot.votes.tgbotapi_extensions.toInlineKeyboardMarkup
+import com.soywiz.klock.DateTime
 import dev.inmo.tgbotapi.bot.RequestsExecutor
 import dev.inmo.tgbotapi.extensions.api.answers.answerCallbackQuery
 import dev.inmo.tgbotapi.extensions.api.edit.ReplyMarkup.editMessageReplyMarkup
+import dev.inmo.tgbotapi.types.CallbackQuery.InlineMessageIdDataCallbackQuery
 import dev.inmo.tgbotapi.types.CallbackQuery.MessageDataCallbackQuery
 import dev.inmo.tgbotapi.types.CallbackQuery.MessageGameShortNameCallbackQuery
 import dev.inmo.tgbotapi.types.ChatId
-import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
+import dev.inmo.tgbotapi.types.CommonUser
+import dev.inmo.tgbotapi.types.UserId
+import dev.inmo.tgbotapi.types.chat.GroupChatImpl
+import dev.inmo.tgbotapi.types.message.CommonGroupContentMessageImpl
 import dev.inmo.tgbotapi.types.message.content.TextContent
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -44,23 +48,29 @@ internal class VotingProcessorTest {
     }
 
     @Test
-    @Disabled
-    fun processCallbackQuery() = runBlocking {
-        val callbackQuery = mockk<MessageDataCallbackQuery> {
-            every { id } returns ""
-            every { user } returns mockk {
-                every { id } returns ChatId(42L)
-            }
-            every { chatInstance } returns ""
-            every { message.hint(ContentMessage::class) } returns mockk<ContentMessage<*>> {
-                every { messageId } returns 1L
-                every { content.hint(TextContent::class) } returns TextContent("")
-                every { chat } returns mockk {
-                    every { id } returns ChatId(1L)
-                }
-            }
-            every { data } returns "TEST-42:+"
-        }
+    fun processMessageDataCallbackQuery() = runBlocking {
+        val message = CommonGroupContentMessageImpl(
+            chat = GroupChatImpl(
+                id = ChatId(1L),
+                title = "Test"
+            ),
+            messageId = 1L,
+            user = CommonUser(UserId(2L), "Test 2"),
+            date = DateTime.now(),
+            forwardInfo = null,
+            editDate = null,
+            replyTo = null,
+            replyMarkup = null,
+            content = TextContent(""),
+            senderBot = null,
+        )
+        val callbackQuery = MessageDataCallbackQuery(
+            id = "",
+            user = CommonUser(UserId(1L), "Test 1"),
+            chatInstance = "",
+            message = message,
+            data = "TEST-42:+"
+        )
 
         coEvery { votesDAO.get("TEST-42") } returns Votes("TEST-42", listOf("+", "-"))
         coEvery { votesDAO.save(any()) } just runs
@@ -68,12 +78,12 @@ internal class VotingProcessorTest {
 
         sut.processCallbackQuery(callbackQuery)
 
-        coVerify(exactly = 1) { votesDAO.save(Votes("TEST-42", listOf("+", "-"), mapOf("42" to "+"))) }
+        coVerify(exactly = 1) { votesDAO.save(Votes("TEST-42", listOf("+", "-"), mapOf("1" to "+"))) }
         coVerify(exactly = 1) { bot.answerCallbackQuery(callbackQuery) }
         coVerify(exactly = 1) {
             bot.editMessageReplyMarkup(
-                message = callbackQuery.message,
-                replyMarkup = Votes("TEST-42", listOf("+", "-"), mapOf("42" to "+")).toInlineKeyboardMarkup()
+                message = message,
+                replyMarkup = Votes("TEST-42", listOf("+", "-"), mapOf("1" to "+")).toInlineKeyboardMarkup(),
             )
         }
 
@@ -81,13 +91,80 @@ internal class VotingProcessorTest {
     }
 
     @Test
-    @Disabled
-    fun processCallbackQueryForNewVotes() {
-        TODO()
+    fun processCallbackQueryForNewVotes() = runBlocking {
+        val message = CommonGroupContentMessageImpl(
+            chat = GroupChatImpl(
+                id = ChatId(1L),
+                title = "Test"
+            ),
+            messageId = 1L,
+            user = CommonUser(UserId(2L), "Test 2"),
+            date = DateTime.now(),
+            forwardInfo = null,
+            editDate = null,
+            replyTo = null,
+            replyMarkup = null,
+            content = TextContent(""),
+            senderBot = null,
+        )
+        val callbackQuery = MessageDataCallbackQuery(
+            id = "",
+            user = CommonUser(UserId(1L), "Test 1"),
+            chatInstance = "",
+            message = message,
+            data = "TEST-42:+"
+        )
+
+        coEvery { votesDAO.get("TEST-42") } returns null
+        every { votesConstructor.invoke("TEST-42") } returns Votes("TEST-42", listOf("+", "-", "="))
+        coEvery { votesDAO.save(any()) } just runs
+        coEvery { bot.answerCallbackQuery(callbackQuery) } returns true
+
+        sut.processCallbackQuery(callbackQuery)
+
+        coVerify(exactly = 1) { votesDAO.save(Votes("TEST-42", listOf("+", "-", "="), mapOf("1" to "+"))) }
+        coVerify(exactly = 1) { bot.answerCallbackQuery(callbackQuery) }
+        coVerify(exactly = 1) {
+            bot.editMessageReplyMarkup(
+                message = message,
+                replyMarkup = Votes("TEST-42", listOf("+", "-", "="), mapOf("1" to "+")).toInlineKeyboardMarkup(),
+            )
+        }
+
+        clearAllMocks()
     }
 
     @Test
-    fun processNonMessageDataCallbackQuery() = runBlocking {
+    fun processInlineMessageIdDataCallbackQuery() = runBlocking {
+        val callbackQuery = InlineMessageIdDataCallbackQuery(
+            id = "",
+            user = CommonUser(UserId(1L), "Test 1"),
+            chatInstance = "",
+            inlineMessageId = "300",
+            data = "TEST-42:+"
+        )
+
+        coEvery { votesDAO.get("TEST-42") } returns null
+        every { votesConstructor.invoke("TEST-42") } returns Votes("TEST-42", listOf("+", "-"))
+        coEvery { votesDAO.save(any()) } just runs
+        coEvery { bot.answerCallbackQuery(callbackQuery) } returns true
+
+        sut.processCallbackQuery(callbackQuery)
+
+        coVerify(exactly = 1) { votesDAO.save(Votes("TEST-42", listOf("+", "-"), mapOf("1" to "+"))) }
+        coVerify(exactly = 1) { bot.answerCallbackQuery(callbackQuery) }
+        coVerify(exactly = 1) {
+            bot.editMessageReplyMarkup(
+                inlineMessageId = "300",
+                replyMarkup = Votes("TEST-42", listOf("+", "-"), mapOf("1" to "+")).toInlineKeyboardMarkup(),
+            )
+        }
+
+        clearAllMocks()
+    }
+
+    @Test
+    fun processUnknownCallbackQuery() = runBlocking {
         sut.processCallbackQuery(
             MessageGameShortNameCallbackQuery(
                 id = "test",
