593d8c52cc77e715ceac6f175ff1d5a6388e2b26
madhead <siarhei.krukau@gmail.com>
2021-10-16 18:17:30 +0200

    Pin request parser

---
+++ pins/src/main/kotlin/by/jprof/telegram/bot/pins/model/PinRequest.kt
@@ -0,0 +1,30 @@
+package by.jprof.telegram.bot.pins.model
+
+import dev.inmo.tgbotapi.types.User
+import dev.inmo.tgbotapi.types.chat.abstracts.Chat
+import dev.inmo.tgbotapi.types.message.abstracts.Message
+import java.time.Duration
+import kotlin.math.ceil
+
+sealed interface PinDuration {
+    object Unrecognized : PinDuration
+
+    data class Recognized(
+        val duration: Duration,
+    ) : PinDuration
+}
+
+data class PinRequest(
+    val message: Message?,
+    val user: User,
+    val chat: Chat,
+    val request: Message,
+    val duration: PinDuration,
+) {
+    // 1h == 1 PIN, rounded up
+    val price: Int
+        get() = when (duration) {
+            is PinDuration.Recognized -> ceil(duration.duration.seconds / 3600.0).toInt()
+            PinDuration.Unrecognized -> throw IllegalArgumentException("Cannot calculate price for unrecognized duration!")
+        }
+}
